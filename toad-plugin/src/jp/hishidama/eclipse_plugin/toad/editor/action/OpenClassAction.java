1978874bd69b242be2bb68cee252d41f7285e3ab
hishidama <hishi.dama@asahi.email.ne.jp>
2013-12-28 09:13:08 +0900

    fixed #9. ダイアグラムからJavaソースを開けるよう修正

--- toad-plugin/src/jp/hishidama/eclipse_plugin/toad/editor/action/OpenClassAction.java
+++ toad-plugin/src/jp/hishidama/eclipse_plugin/toad/editor/action/OpenClassAction.java
@@ -1,23 +1,31 @@
 package jp.hishidama.eclipse_plugin.toad.editor.action;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import jp.hishidama.eclipse_plugin.jdt.util.TypeUtil;
 import jp.hishidama.eclipse_plugin.toad.editor.ToadEditor;
+import jp.hishidama.eclipse_plugin.toad.model.diagram.Diagram;
+import jp.hishidama.eclipse_plugin.toad.model.diagram.DiagramEditPart;
 import jp.hishidama.eclipse_plugin.toad.model.node.ClassNameNode;
 import jp.hishidama.eclipse_plugin.toad.model.node.NodeElement;
 import jp.hishidama.eclipse_plugin.toad.model.node.NodeElementEditPart;
 import jp.hishidama.eclipse_plugin.toad.model.node.operator.OperatorNode;
+import jp.hishidama.eclipse_plugin.util.StringUtil;
 import jp.hishidama.xtext.dmdl_editor.ui.internal.LogUtil;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.gef.ui.actions.SelectionAction;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IEditorPart;
 
 public class OpenClassAction extends SelectionAction {
 	public static final String ID = "TOAD_OPEN_CLASS";
@@ -55,6 +63,10 @@ public class OpenClassAction extends SelectionAction {
 					ClassNameNode node = (ClassNameNode) model;
 					result.add(node);
 				}
+			} else if (obj instanceof DiagramEditPart) {
+				DiagramEditPart part = (DiagramEditPart) obj;
+				Diagram model = part.getModel();
+				result.add(model);
 			}
 		}
 		return result;
@@ -66,7 +78,12 @@ public class OpenClassAction extends SelectionAction {
 		List<ClassNameNode> list = getSelectedObjects();
 		for (ClassNameNode node : list) {
 			try {
-				IType type = javaProject.findType(node.getClassName());
+				String className = node.getClassName();
+				if (StringUtil.isEmpty(className)) {
+					MessageDialog.openError(null, "DSL (Java class) open error", "クラス名を入力して下さい。");
+					return;
+				}
+				IType type = javaProject.findType(className);
 				if (node instanceof OperatorNode) {
 					OperatorNode operator = (OperatorNode) node;
 					IMethod method = TypeUtil.findMethod(type, operator.getMethodName());
@@ -78,10 +95,16 @@ public class OpenClassAction extends SelectionAction {
 						}
 					}
 				}
-				JavaUI.openInEditor(type);
+				IEditorPart part = JavaUI.openInEditor(type);
+				if (part == null) {
+					String message = MessageFormat.format("Javaソースファイルが存在しません。\nclassName={0}\ntype={1}", className,
+							type);
+					MessageDialog.openError(null, "DSL (Java class) open error", message);
+				}
 				return;
 			} catch (Exception e) {
-				LogUtil.logError("", e);
+				IStatus status = LogUtil.logError("Java class open error", e);
+				ErrorDialog.openError(null, "DSL (Java class) open error", "Javaソースを開く際に例外が発生しました。", status);
 			}
 		}
 	}
